b8689acf5a7af283cd2108d95a38fd2465e6df0e
Pranav-here <113384349+Pranav-here@users.noreply.github.com>
2025-06-13 08:00:24 +0530

    v1.2
    Setup llm and tools

--- code/ai_agent.py
+++ code/ai_agent.py
@@ -6,6 +6,13 @@ TAVILY_API_KEY = os.environ.get("TAVILY_API_KEY")
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
 # Step 2: Setup llm and tools
+from langchain_groq import chat_groq
+from langchain_openai import ChatOpenAI
+from langchain_community.tools import TavilySearchResults
 
+openai_llm=ChatOpenAI(model='gpt-4o-mini')
+groq_llm=chat_groq(model='llama-3.3-70b-versatile')
+
+search_tool = TavilySearchResults(max_results=2)  # 
 
 # Step 3: Setup AI Agent with search tool functionality 
