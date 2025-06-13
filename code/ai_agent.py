d053b49ac800d9648be19c88b6a396008fbeb077
Pranav-here <113384349+Pranav-here@users.noreply.github.com>
2025-06-13 08:48:38 +0530

    v2
    Fixed key errors

--- code/ai_agent.py
+++ code/ai_agent.py
@@ -6,27 +6,27 @@ TAVILY_API_KEY = os.environ.get("TAVILY_API_KEY")
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
 # Step 2: Setup llm and tools
-from langchain_groq import chat_groq
+from langchain_groq import ChatGroq
 from langchain_openai import ChatOpenAI
-from langchain_community.tools import TavilySearchResults
+from langchain_tavily import TavilySearch
 
 openai_llm=ChatOpenAI(model='gpt-4o-mini')
-groq_llm=chat_groq(model='llama-3.3-70b-versatile')
+groq_llm=ChatGroq(model='llama-3.3-70b-versatile')
 
-search_tool = TavilySearchResults(max_results=2)  # 
+search_tool = TavilySearch(max_results=2)
 
 # Step 3: Setup AI Agent with search tool functionality 
 
 system_prompt="Act as an AI chatbot which is smart"
 
-from langgraph.prebuilt import creat_react_agent
-agent=creat_react_agent(
+from langgraph.prebuilt import create_react_agent
+agent=create_react_agent(
     model=groq_llm,
     tools=[search_tool],
-    state_modifier=system_prompt  # This is for the agent role like analyst, stock broker
+    prompt=system_prompt  # This is for the agent role like analyst, stock broker
 )
 
 query="Tell me about the trends in crypto markets"
 state={"messages": query}
 response=agent.invoke(state)
-print()
+print(response)
