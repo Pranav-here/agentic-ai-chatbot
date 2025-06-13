caef419297f3b77837d444c399695e2c766f57fb
Pranav-here <113384349+Pranav-here@users.noreply.github.com>
2025-06-13 17:44:11 +0530

    Final MVP

--- code/frontend.py
+++ code/frontend.py
@@ -22,9 +22,25 @@ allow_web_search=st.checkbox("Allow to search the web")
 
 user_query=st.text_area("Enter your Query", height=150, placeholder="Ask Anything!!!")
 
+API_URL="http://127.0.0.1:9999/chat"
+
 if st.button("Ask Agent!"):
     if user_query.strip():
-        # Get some backend respond.
-        response = "Hi, I'm dummy"
-        st.subheader("Agent Response")
-        st.markdown(f"**Final Respnose:** {response}")
+        import requests
+
+        payload = {
+            "model_name": selected_model,
+            "model_provider": provider,
+            "system_prompt": system_prompt,
+            "messages": [user_query],
+            "allow_search": allow_web_search
+        }
+
+        response = requests.post(API_URL, json=payload)
+        if response.status_code == 200:
+            response_data = response.json()
+            if "error" in response_data:
+                st.error(response_data["error"])
+            else:
+                st.subheader("Agent Response")
+                st.markdown(f"**Final Response:** {response_data}")
