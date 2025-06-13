b85d4b832f6b2caa191b670aa87545ff44692f7e
Pranav-here <113384349+Pranav-here@users.noreply.github.com>
2025-06-13 08:09:51 +0530

    v1.3
    Setup AI agent with search tool functionality

--- code/ai_agent.py
+++ code/ai_agent.py
@@ -16,3 +16,17 @@ groq_llm=chat_groq(model='llama-3.3-70b-versatile')
 search_tool = TavilySearchResults(max_results=2)  # 
 
 # Step 3: Setup AI Agent with search tool functionality 
+
+system_prompt="Act as an AI chatbot which is smart"
+
+from langgraph.prebuilt import creat_react_agent
+agent=creat_react_agent(
+    model=groq_llm,
+    tools=[search_tool],
+    state_modifier=system_prompt  # This is for the agent role like analyst, stock broker
+)
+
+query="Tell me about the trends in crypto markets"
+state={"messages": query}
+response=agent.invoke(state)
+print()
